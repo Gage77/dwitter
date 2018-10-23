da29c8caa0fc46f4977934b66fc031f1282d973b
Gage77 <huntergblack@gmail.com>
2018-10-23 14:18:16 -0500

    transfered back to sqlite for pythonanywhere deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Will be using postgres 'dwitter' database on localhost server
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dwitter',
-        'USER': 'postgres',
-        'PASSWORD': '10488000Aa',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
